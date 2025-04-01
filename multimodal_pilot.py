2146d4220809b606e947af22082464aed6f7ed86
dueuwh <usang0821@naver.com>
2025-04-02 04:49:38 +0900

    delete: multimodal_pilot.py is redundant, main.py does the job of this file

--- multimodal_pilot.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from utils.dataloader import MultiModalDataset
-from utils.preprocessing import bandpassfilter
